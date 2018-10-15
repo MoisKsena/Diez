6b6759b3de3bed8dd60e32568e68dfa8b70942cb
Kseniia Moiseeva <mailtoksena@gmail.com>
2018-10-15 14:15:18 +0300

    @media (min-width: 1000px) nav-menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-// Переключение классов на кнопке-бургере и на меню
-var toggler = document.getElementById('toggler');
-toggler.addEventListener('click', mainNavVisibleToggle);
-function mainNavVisibleToggle(e) {
-  e.preventDefault();
-  toggler.classList.toggle('toggler--close');
-  document.getElementById('main-nav').classList.toggle('main-nav--visible');
-}
+var Menu = {
+  
+  el: {
+    ham: $('.menu'),
+    menuTop: $('.menu-top'),
+    menuMiddle: $('.menu-middle'),
+    menuBottom: $('.menu-bottom')
+  },
+  
+  init: function() {
+    Menu.bindUIactions();
+  },
+  
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+      }
+    );
+  },
+  
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('menu-top-click');
+    Menu.el.menuMiddle.toggleClass('menu-middle-click');
+    Menu.el.menuBottom.toggleClass('menu-bottom-click'); 
+  }
+};
+
+Menu.init();

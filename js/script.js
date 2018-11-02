cbb55049535d4d24910b1fb8a7b4aa3b6ee99d6b
Kseniia Moiseeva <mailtoksena@gmail.com>
2018-11-02 23:47:41 +0300

    fix slider // map // burger

--- js/script.js
+++ js/script.js
@@ -9,22 +9,60 @@ $(document).ready(function(){
 	});
 
 	function checkSlider(){
-		
-	if( $(window).width() < 1200 ) {
-		if( slickIsActive === false ) {
-			$('#slider').slick({
-				autoplay: true,
-			});
-			slickIsActive = true;
+		if( $(window).width() < 1200 ) {
+			if( slickIsActive === false ) {
+				$('#slider').slick({
+					autoplay: true,
+				});
+				slickIsActive = true;
+			}
 		}
-	}
-	else {
-		if( slickIsActive === true ) {
-			$('#slider').slick('unslick');
-			slickIsActive = false;
+		else {
+			if( slickIsActive === true ) {
+				$('#slider').slick('unslick');
+				slickIsActive = false;
+			}
+			
 		}
-		
 	}
 
-	}
+
+
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.94199306418127,30.279423499999996],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemarkWithContent = new ymaps.Placemark([59.94199306418127,30.279423499999996], {
+            hintContent: 'Собственный значок метки с контентом',
+            iconContent: ''
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/geotag.png',
+            // Размеры метки.
+            iconImageSize: [38, 49],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-19, -49],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemarkWithContent);
+});
 });
